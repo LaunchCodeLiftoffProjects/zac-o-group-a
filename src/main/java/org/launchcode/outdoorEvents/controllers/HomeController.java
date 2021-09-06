d5d826f2041879edc20ddfd51c907068a43d0ce3
Hitz325 <thitschler88@gmail.com>
2021-09-05 21:22:15 -0500

    Worked more on the User class and UserController. Also added a registration link to the home page with a successful landing page after. Want to work on login feature next.

--- src/main/java/org/launchcode/outdoorEvents/controllers/HomeController.java
+++ src/main/java/org/launchcode/outdoorEvents/controllers/HomeController.java
@@ -1,15 +1,35 @@
 package org.launchcode.outdoorEvents.controllers;
 
+import org.launchcode.outdoorEvents.data.UserRepository;
+import org.launchcode.outdoorEvents.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("")
 public class HomeController {
 
+        @Autowired
+        private UserRepository userRepository;
+
         @GetMapping("")
-        public String index() {return "index";    }
+        public String index() {return "index";}
+
+        @GetMapping("/register")
+        public String showSignUpForm(Model model){
+                model.addAttribute("user", new User());
+                return "/user/signup_form";
+        }
+
+        @PostMapping("/process_registration")
+        public String processRegistration(User user){
+                userRepository.save(user);
+
+                return "/user/registration_success";
+        }
 
 }
--- src/main/java/org/launchcode/outdoorEvents/controllers/UserController.java
+++ src/main/java/org/launchcode/outdoorEvents/controllers/UserController.java
@@ -12,29 +12,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("user")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
 
-    @GetMapping("add")
-    public String displayAddUserForm() {
-        return "user/add";
-    }
 
-    @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        //model.addAttribute("username", user.getUsername());
-        if (user.getPassword().equals(verify)) {
-            userRepository.save(user);
-            return "user/index";
-        }
-        else {
-            model.addAttribute("error", "Passwords do not match");
-            return "user/add";
-        }
-
-    }
 
 
 }
