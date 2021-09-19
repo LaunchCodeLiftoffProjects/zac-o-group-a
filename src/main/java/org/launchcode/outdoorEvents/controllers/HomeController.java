abaf210a3ae34f1265a3b173d1fdf321810b6b2d
Hitz325 <thitschler88@gmail.com>
2021-09-19 14:40:46 -0500

    Figured out how to pull fields from the User object and display on the index page after login. Probably going to pause on this branch for a bit to work on the list.

--- src/main/java/org/launchcode/outdoorEvents/controllers/HomeController.java
+++ src/main/java/org/launchcode/outdoorEvents/controllers/HomeController.java
@@ -1,12 +1,13 @@
 package org.launchcode.outdoorEvents.controllers;
 
 import org.launchcode.outdoorEvents.data.UserRepository;
-import org.launchcode.outdoorEvents.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("")
@@ -15,14 +16,10 @@ public class HomeController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/")
-    public String index(Model model){
 
-        User currentUser = userRepository.findByUsername("hitz");
-        //TODO: Figure out how to do this to the currently logged in user.
-        String firstName = currentUser.getFirstName();
+    @GetMapping("/")
+    public String index(HttpServletRequest request, Model model){
         model.addAttribute("title", "Logger");
-        model.addAttribute("firstName", firstName);
         return "index";
     }
 }
