7713b6b77a26c4bfdd0d3f228c62073704ec6cf3
Hitz325 <thitschler88@gmail.com>
2021-09-30 22:08:38 -0500

    Stuff

--- src/main/java/org/launchcode/outdoorEvents/models/EventCategory.java
+++ src/main/java/org/launchcode/outdoorEvents/models/EventCategory.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.launchcode.outdoorEvents.models;
 
 import javax.persistence.Entity;
@@ -22,23 +21,3 @@ public class EventCategory extends AbstractEntity {
     public EventCategory() {}
 
 }
-=======
-package org.launchcode.outdoorEvents.models;
-
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-@Entity
-public class EventCategory extends AbstractEntity {
-
-    public EventCategory() {}
-
-}
->>>>>>> 6a7d9bd0573104a4846b463685570f2a3eff0f29
