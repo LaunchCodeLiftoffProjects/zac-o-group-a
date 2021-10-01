8297fe50350bbda5ddd2fd072a03709213d0f41c
Thomas Hitschler <80077568+Hitz325@users.noreply.github.com>
2021-09-30 21:13:47 -0500

    Delete Logger directory

--- Logger/src/main/java/org/launchcode/outdoorEvents/data/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package org.launchcode.outdoorEvents.data;
-
-import org.launchcode.outdoorEvents.models.Location;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LocationRepository extends CrudRepository<Location, Integer> {
-}
--- Logger/src/main/resources/static/script.js
+++
@@ -1,3 +0,0 @@
-let appName = "Outdoor Events";
-
-let map;
